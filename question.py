0741a48e3a44bde910f68765a17ffc5ebe64967c
Marcel-Fill <you@example.com>
2021-12-06 11:34:53 +0100

    Update question.py

--- question.py
+++ question.py
@@ -36,44 +36,49 @@ def getQuest(rn):
     ques = question(quest[rn][0], quest[rn][1], quest[rn][awns], quest[rn][awns1], quest[rn][awns2], quest[rn][awns3] )
     return ques
 
-readFile()
-curLvl = 40
-lvl1 = 60
-lvl2 = 66 
-lvl3 = 50
-lvl4 = 15
-run = 1
-sub = 40
-right = 0
-cor = 0
 
-while run == 1: 
-    rn = random.randint(curLvl-sub,curLvl)
-    ques = getQuest(rn)
-    print(ques)
-    awn = input("The awnser is?")
-    if awn == quest[rn][2]:
-        print("YOU DID IT!!!!!!!!!!!!")
-        cor += 1
-        if cor == 10:
-            print("YOU DID IT! YOU WON THE GAME!!!!! \t THE HTL IS SADLY TO POOR TO PAY YOU SO BYE")
-            run = 0
+def main():
+    readFile()
+    curLvl = 40
+    lvl1 = 60
+    lvl2 = 66 
+    lvl3 = 50
+    lvl4 = 15
+    run = 1
+    sub = 40
+    right = 0
+    cor = 0
+
+    while run == 1: 
+        rn = random.randint(curLvl-sub,curLvl)
+        ques = getQuest(rn)
+        print(ques)
+        awn = input("The awnser is?")
+        if awn == quest[rn][2]:
+            print("YOU DID IT!!!!!! ONTO THE NEXT ONE")
+            cor += 1
+            if cor == 10:
+                print("YOU DID IT! YOU WON THE GAME!!!!! \t THE HTL IS SADLY TO POOR TO PAY YOU. Anyways see ya")
+                run = 0
+            else:
+                match cor:
+                    case 2:
+                        curLvl += lvl1
+                        sub = lvl1
+                    case 4:
+                        curLvl += lvl2
+                        sub = lvl2
+                    case 6: 
+                        curLvl += lvl3
+                        sub = lvl3
+                    case 8:
+                        curLvl += lvl4
+                        sub = lvl4 
         else:
-            match cor:
-                case 2:
-                    curLvl += lvl1
-                    sub = lvl1
-                case 4:
-                    curLvl += lvl2
-                    sub = lvl2
-                case 6: 
-                    curLvl += lvl3
-                    sub = lvl3
-                case 8:
-                    curLvl += lvl4
-                    sub = lvl4 
-    else:
-        print("NOPE YOU ARE WRONG!!! \n What do you think about trying again?")
-        cor = 0 
-        curLvl = 40 
-        sub = 40
+            print("NOPE YOU ARE WRONG!!! \n What do you think about trying again?")
+            cor = 0 
+            curLvl = 40 
+            sub = 40
+
+if __name__ == "__main__":
+    main()
